3433b7a1fb301502613b5ebdb555de71d7b0ca8f
ChuiZhao <njuzch0414@gmail.com>
2023-03-22 16:46:41 -0400

    make tome possible

---
+++ tome/patch/timm.py
@@ -0,0 +1,182 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# --------------------------------------------------------
+
+
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+import math
+
+# from timm.models.vision_transformer import Attention, Block, VisionTransformer
+
+from src.models.vit_backbones.vit import Block, Attention
+from src.models.vit_prompt.vit import PromptedTransformer
+
+from tome.merge import bipartite_soft_matching, merge_source, merge_wavg
+from tome.utils import parse_r
+
+
+class ToMeBlock(Block):
+    """
+    Modifications:
+     - Apply ToMe between the attention and mlp blocks
+     - Compute and propogate token size and potentially the token sources.
+    """
+
+    def _drop_path1(self, x):
+        return self.drop_path1(x) if hasattr(self, "drop_path") else x
+
+    def _drop_path2(self, x):
+        return self.drop_path2(x) if hasattr(self, "drop_path") else x
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Note: this is copied from timm.models.vision_transformer.Block with modifications.
+        attn_size = self._tome_info["size"] if self._tome_info["prop_attn"] else None
+        x_attn, metric = self.attn(self.attention_norm(x), attn_size)
+        x = x + self._drop_path1(x_attn)
+
+        r = self._tome_info["r"].pop(0)
+        if r > 0:
+            # Apply ToMe here
+            merge, _ = bipartite_soft_matching(
+                metric,
+                r,
+                self._tome_info["class_token"],
+                self._tome_info["distill_token"],
+            )
+            if self._tome_info["trace_source"]:
+                self._tome_info["source"] = merge_source(
+                    merge, x, self._tome_info["source"]
+                )
+            x, self._tome_info["size"] = merge_wavg(merge, x, self._tome_info["size"])
+
+        x = x + self._drop_path2(self.ffn(self.ffn_norm(x)))
+        return x, x_attn
+
+
+class ToMeAttention(Attention):
+    """
+    Modifications:
+     - Apply proportional attention
+     - Return the mean of k over heads from attention
+    """
+
+    def forward(
+        self, x: torch.Tensor, size: torch.Tensor = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        # Note: this is copied from timm.models.vision_transformer.Attention with modifications.
+        B, N, C = x.shape
+
+
+        mixed_query_layer = self.query(x) # B, num_patches, head_size*num_head
+        mixed_key_layer = self.key(x)
+        mixed_value_layer = self.value(x)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer) # B, num_head, num_patches, head_size
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer) # B, num_head, num_patches, head_size
+
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2)) # B, num_head, num_patches, num_patches
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        if size is not None:
+            attn = attn + size.log()[:, None, None, :, 0]
+        
+        attention_probs = self.softmax(attention_scores) # B, num_head, num_patches(query), num_patches(key)
+        attention_probs = self.attn_dropout(attention_probs)
+
+        context_layer = torch.matmul(attention_probs, value_layer) # B, num_head, num_patches, head_size
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+        attention_output = self.out(context_layer)
+        attention_output = self.proj_dropout(attention_output)
+        return attention_output, key_layer.mean(1)
+
+
+        # qkv = (
+        #     self.qkv(x)
+        #     .reshape(B, N, 3, self.num_heads, C // self.num_heads)
+        #     .permute(2, 0, 3, 1, 4)
+        # )
+        # q, k, v = (
+        #     qkv[0],
+        #     qkv[1],
+        #     qkv[2],
+        # )  # make torchscript happy (cannot use tensor as tuple)
+
+        # attn = (q @ k.transpose(-2, -1)) * self.scale
+
+        # # Apply proportional attention
+        # if size is not None:
+        #     attn = attn + size.log()[:, None, None, :, 0]
+
+        # attn = attn.softmax(dim=-1)
+        # attn = self.attn_drop(attn)
+
+        # x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        # x = self.proj(x)
+        # x = self.proj_drop(x)
+
+        # # Return k as well here
+        # return x, k.mean(1)
+
+
+def make_tome_class(transformer_class):
+    class ToMeVisionTransformer(transformer_class):
+        """
+        Modifications:
+        - Initialize r, token size, and token sources.
+        """
+
+        def forward(self, *args, **kwdargs) -> torch.Tensor:
+            self._tome_info["r"] = parse_r(len(self.enc.transformer.encoder.layer), self.r)
+            self._tome_info["size"] = None
+            self._tome_info["source"] = None
+
+            return super().forward(*args, **kwdargs)
+
+    return ToMeVisionTransformer
+
+
+def apply_patch(
+    model: PromptedTransformer, trace_source: bool = False, prop_attn: bool = True
+):
+    """
+    Applies ToMe to this transformer. Afterward, set r using model.r.
+
+    If you want to know the source of each token (e.g., for visualization), set trace_source = true.
+    The sources will be available at model._tome_info["source"] afterward.
+
+    For proportional attention, set prop_attn to True. This is only necessary when evaluating models off
+    the shelf. For trianing and for evaluating MAE models off the self set this to be False.
+    """
+    ToMeVisionTransformer = make_tome_class(model.__class__)
+
+    model.__class__ = ToMeVisionTransformer
+    model.r = 0
+    model._tome_info = {
+        "r": model.r,
+        "size": None,
+        "source": None,
+        "trace_source": trace_source,
+        "prop_attn": prop_attn,
+        "class_token": True,
+        "distill_token": False,
+    }
+    if hasattr(model, "dist_token") and model.dist_token is not None:
+        model._tome_info["distill_token"] = True
+
+    for module in model.modules():
+        if isinstance(module, Block):
+            module.__class__ = ToMeBlock
+            module._tome_info = model._tome_info
+        elif isinstance(module, Attention):
+            module.__class__ = ToMeAttention
